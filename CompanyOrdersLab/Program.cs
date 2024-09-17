95a705ae28f2b674cd2cb8f859a9da76f9dfbf4d
Shivashankar Biradar <Shivashankar.Biradar@dominos.co.uk>
2024-09-17 17:19:56 +0530

    Add project files.

---
+++ CompanyOrdersLab/Program.cs
@@ -0,0 +1,109 @@
+﻿
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Company company = new Company();
+
+        Customer c1 = new Customer();
+        Customer c2 = new Customer();
+
+        company.Customers.Add(c1);
+        company.Customers.Add(c2);
+
+        //RegisterCustomer registerCustomer1 = new RegisterCustomer();
+        //RegisterCustomer registerCustomer2 = new RegisterCustomer();
+
+        //company.Customers.Add(registerCustomer1);
+        //company.Customers.Add(registerCustomer2);
+
+        Console.WriteLine($"No. of all customer in a company: {company.Customers.Count}");
+        // Console.WriteLine($"No. of normal customer in a company: {company.Customers.Count}");
+        // Console.WriteLine($"No. of registered customer in a company: {company.Customers.Count}");
+
+
+        Order order1 = new Order();
+        c1.Orders.Add(order1);
+
+        Order order2 = new Order();
+        c2.Orders.Add(order2);
+
+        Item item1 = new Item() { Description = "IPhone", Rate = 75000 };
+        OrderItem orderItem1 = new OrderItem();
+        orderItem1.Item = item1;
+        orderItem1.Quantity = 2;       
+
+        Item item2 = new Item() { Description = "Galaxy Watch", Rate = 15000 };
+        OrderItem orderItem2 = new OrderItem() { Item = item2, Quantity = 5 };
+
+        //company.Items.Add(item1);
+        //company.Items.Add(item2);
+
+        order1.OrderItems.Add(orderItem1);
+        order2.OrderItems.Add(orderItem2);
+
+        Console.WriteLine(company.GetTotalWorthOfOrdersPlaced());
+    }
+}
+
+class Company
+{
+    public List<Customer> Customers { get; set; } = new List<Customer>();
+
+    public List<Item> Items { get; set; } = new List<Item>();
+    public double GetTotalWorthOfOrdersPlaced()
+    {
+        double total = 0;
+        foreach (Customer customer in Customers)
+        {
+            var data = customer.Orders.FirstOrDefault();
+
+            var item = data.OrderItems.FirstOrDefault().Item.Rate;
+
+            var itemQty = data.OrderItems.FirstOrDefault().Quantity;
+
+            total += itemQty * item;
+
+        }
+        
+
+        // TO DO..
+
+        return total;
+    }
+}
+
+public class Item
+{
+    public string? Description { get; set; }
+
+    public double Rate { get; set; }
+}
+
+public class Customer
+{
+    public List<Order> Orders { get; set; } = new List<Order>();
+}
+
+public class OrderItem
+{
+    public Item Item { get; set; } = new Item();
+    public int Quantity { get; set; }
+
+}
+
+public class Order // Is - A relationship
+{
+    public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
+
+public class RegisterCustomer : Customer
+{
+    public double SpecialDiscount { get; set; }
+
+    //public double GetSpecialDiscount()
+    //{
+    //    return SpecialDiscount;
+    //}
+}
---
+++ TrainingAppLab/Program.cs
@@ -0,0 +1,93 @@
+﻿
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+    }
+}
+
+class Organization
+{
+    public string? Name { get; set; }
+
+    public string GetName()
+    {
+        return "";
+    }
+}
+
+class Trainer
+{
+    public Organization Organization { get; set; } = new Organization();
+
+    public List<Training> Training { get; set; } = new List<Training>();
+
+    public Trainee Trainee { get; set; } = new Trainee();
+
+    public string GetOrganization()
+    {
+        return "";
+    }
+}
+
+class Trainee
+{
+    public List<Training> Training { get; set; } = new List<Training>();
+}
+
+abstract class Training
+{
+    public Course? Course { get; set; }
+
+    public int GetNumberOfTrainee()
+    {
+        return 0;
+    }
+
+    public string GetTrainingOrganizationName()
+    {
+        return "Test";
+    }
+
+    public int GetTrainingDurationInHrs()
+    {
+        return 1;
+    }
+}
+
+class Course
+{
+    public List<Module> Modules = new List<Module>();
+    public List<Module> GetCourses()
+    {
+        return Modules;
+    }
+}
+
+class Module
+{
+    public List<Unit> Units = new List<Unit>();
+    public List<Unit> GetUnits()
+    {
+        return Units;
+    }
+}
+
+class Unit
+{
+    public int DurationHrs { get; set; }
+
+    public List<Topic> Topics = new List<Topic>();
+
+    public int GetDuration()
+    {
+        return DurationHrs;
+    }
+}
+
+class Topic
+{
+    public string? Name { get; set; }
+}
