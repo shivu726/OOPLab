4edaeb9d66643c30f0704017a5ab76a396974293
Shivashankar Biradar <Shivashankar.Biradar@dominos.co.uk>
2024-10-05 21:56:50 +0530

    added methods

--- CoolProductsAPIService/Models/Entities/Products.cs
+++ CoolProductsAPIService/Models/Entities/Products.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CoolProductsAPIService.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoolProductsAPIService.Models.Entities
 {
@@ -22,3 +25,55 @@ namespace CoolProductsAPIService.Models.Entities
         public bool InStock { get; set; }
     }
 }
+
+
+//[HttpPost]
+//public IActionResult Post(Products products)
+//{
+//    if (!ModelState.IsValid)
+//    {
+//        return BadRequest("Invalid input.");
+//    }
+//    _context.Products.Add(products);
+//    _context.SaveChanges();
+
+//    return Created($"api/products/{products.ProductId}", products);
+//}
+
+//[HttpDelete]
+//[Route("{id}")]
+//public IActionResult DeleteProduct(int id)
+//{
+//    var prod = _context.Products.Find(id);
+//    if (prod == null)
+//    {
+//        return NotFound();
+//    }
+//    else
+//    {
+//        _context.Products.Remove(prod);
+//        _context.SaveChanges();
+
+//        return Ok();
+//    }
+//}
+
+//[HttpPut]
+//[Route("")]
+//public IActionResult UpdateProduct([FromHeader] int id, [FromBody] Products products)
+//{
+//    //var data = _context.Products.Find(id);
+//    //if (data == null)
+//    //{
+//    //    return NotFound();
+//    //}
+
+//    if (!ModelState.IsValid)
+//    {
+//        return BadRequest("Invalid input.");
+//    }
+
+//    _context.Products.Update(products);
+//    _context.SaveChanges();
+//    return Ok();
+//}
